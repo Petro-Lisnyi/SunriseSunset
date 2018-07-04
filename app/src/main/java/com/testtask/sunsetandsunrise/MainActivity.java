5a3bbec8507bf1d6f824760a29db6df504368bfd
Petro <petro.lisnyi@gmail.com>
2018-07-04 09:46:21 +0300

    Added feature for searching for any city

--- app/src/main/java/com/testtask/sunsetandsunrise/MainActivity.java
+++ app/src/main/java/com/testtask/sunsetandsunrise/MainActivity.java
@@ -16,14 +16,20 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.common.api.Status;
 import com.google.android.gms.location.places.GeoDataClient;
 import com.google.android.gms.location.places.Place;
 import com.google.android.gms.location.places.PlaceDetectionClient;
 import com.google.android.gms.location.places.PlaceLikelihoodBufferResponse;
 import com.google.android.gms.location.places.Places;
+import com.google.android.gms.location.places.ui.PlaceAutocomplete;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 
@@ -32,6 +38,8 @@ public class MainActivity extends AppCompatActivity {
 
     private final int PERMISSIONS_REQUEST_LOCATION = 1;
     private boolean mLocationPermission;
+
+    private final static int PLACE_AUTOCOMPLETE_REQUEST_CODE = 0;
 
     private float mCurrentLat;
     private float mCurrentLng;
@@ -45,7 +53,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        mToolbar = findViewById(R.id.toolbar);
         setSupportActionBar(mToolbar);
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && isLocationPermissionRestricted()) {
@@ -66,10 +74,27 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == PLACE_AUTOCOMPLETE_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                Place place = PlaceAutocomplete.getPlace(this, data);
+                Log.i(TAG, "Current place: " + place.getName() + " with latitude - " +
+                        place.getLatLng().latitude + ", longitude - " + place.getLatLng().longitude);
+                // TODO: Handle the result.
+            } else if (resultCode == PlaceAutocomplete.RESULT_ERROR) {
+                Status status = PlaceAutocomplete.getStatus(this, data);
+                // TODO: Handle the error.
+                Log.e(TAG, status.getStatusMessage());
+
+            } else if (resultCode == RESULT_CANCELED) {
+                // The user canceled the operation.
+            }
+        }
     }
 
     @RequiresApi(api = Build.VERSION_CODES.M)
@@ -110,6 +135,18 @@ public class MainActivity extends AppCompatActivity {
         requestPermissions(permissions, PERMISSIONS_REQUEST_LOCATION);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.action_search) callPlaceAutocomplete();
+        return super.onOptionsItemSelected(item);
+    }
+
     private boolean isLocationPermissionRestricted() {
         return !(ActivityCompat.checkSelfPermission(getApplicationContext(),
                 Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED);
@@ -123,18 +160,36 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onComplete(@NonNull Task<PlaceLikelihoodBufferResponse> task) {
                 try {
-                    Place pl = task.getResult().get(0).getPlace();
-                    mCurrentLat = (float) pl.getLatLng().latitude;
-                    mCurrentLng = (float) pl.getLatLng().longitude;
+                    Place place = task.getResult().get(0).getPlace();
+                    mCurrentLat = (float) place.getLatLng().latitude;
+                    mCurrentLng = (float) place.getLatLng().longitude;
 
-                    Log.i(TAG, "Current place: " + pl.getName() + " with latitude - " +
-                    mCurrentLat + ", longitude - " + mCurrentLng + ".");
+                    Log.i(TAG, "Current place: " + place.getName() + " with latitude - " +
+                            mCurrentLat + ", longitude - " + mCurrentLng + ".");
                 } catch (SecurityException e) {
+                    Log.e(TAG, e.getMessage());
+                    Toast.makeText(getApplicationContext(), e.getLocalizedMessage(),
+                            Toast.LENGTH_SHORT).show();
+                } catch (Exception e){
                     Log.e(TAG, e.getMessage());
                     Toast.makeText(getApplicationContext(), e.getLocalizedMessage(),
                             Toast.LENGTH_SHORT).show();
                 }
             }
         });
+    }
+
+    private void callPlaceAutocomplete() {
+        try {
+            Intent intent = new PlaceAutocomplete.IntentBuilder(PlaceAutocomplete.MODE_OVERLAY)
+                    .build(this);
+            startActivityForResult(intent, PLACE_AUTOCOMPLETE_REQUEST_CODE);
+        } catch (GooglePlayServicesRepairableException e) {
+            e.printStackTrace();
+            Log.e(TAG, e.getMessage());
+        } catch (GooglePlayServicesNotAvailableException e) {
+            e.printStackTrace();
+            Log.e(TAG, e.getMessage());
+        }
     }
 }
